13a316cbe07e398bc7077fbc66bafff9af93b126
BenBerkenbosch <ben.berkenbosch@student.graafschapcollege.nl>
2021-05-21 11:32:23 +0200

    Ewaa Niffo's

---
+++ public/lbleagueoflegends.php
@@ -0,0 +1,58 @@
+<?php
+    require_once("header.php");
+?>
+
+<style>
+    .MKContainer {
+        background-image: url(Images/LeagueOfLegendsBG.jpg);
+        background-repeat: no-repeat;
+    }
+
+.vertical-menu {
+  width: 200px;
+  height: 150px;
+  overflow-y: auto;
+}
+
+.vertical-menu img {
+  width: 25px;
+  height: 25px;
+  float: right;
+}
+
+.vertical-menu a {
+  background-color: #eee;
+  color: black;
+  display: block;
+  padding: 12px;
+  text-decoration: none;
+}
+
+.vertical-menu a:hover {
+  background-color: #ccc;
+}
+
+.vertical-menu a.active {
+  background-color: red;
+  color: white;
+}
+
+p {
+  opacity: 0%;
+}
+</style>
+<div class="flex justify-center MKContainer">
+
+    <div class="vertical-menu">
+  <a href="#"><img src="Images/user.svg" alt="">Ben</a>
+  <a href="#"><img src="Images/user.svg" alt="">Erik</a>
+  <a href="#"><img src="Images/user.svg" alt="">Keagan</a>
+  <a href="#"><img src="Images/user.svg" alt="">Tim</a>
+  <a href="#"><img src="Images/user.svg" alt="">Duncan</a>
+    </div>
+
+    <p><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>a</p>
+
+</div>
+
+
